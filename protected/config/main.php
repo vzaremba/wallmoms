b274111410a33d99ebd6748e0aa30292ca3af3d4
Zaremba <vzaremba@ws-48.ua.sphere-inc.lan>
2013-04-03 14:22:04 +0300

    WM: added YiiBooster

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Yii Blog Demo',
+	'name'=>'WallMoms',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
