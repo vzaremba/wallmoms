6fd6c67de7a8812e8748541f18d6af8b600c236f
Zaremba <vzaremba@ws-48.ua.sphere-inc.lan>
2013-04-03 15:39:28 +0300

    WM: added YiiBooster step n

--- protected/config/main.php
+++ protected/config/main.php
@@ -11,6 +11,7 @@ Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'WallMoms',
+    'theme'=>'bootstrap',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
