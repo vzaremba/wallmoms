e2df099c09dd288570fe23e49fa8ae1357c7744c
Zaremba <vzaremba@ws-48.ua.sphere-inc.lan>
2013-04-04 17:53:49 +0300

    WM: social reg - step 3

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -113,7 +113,32 @@ class SiteController extends Controller
             $this->redirect(array('login'));
         }
 
+
+
+        
         // default authorization code through login/password ..
+        if (!defined('CRYPT_BLOWFISH')||!CRYPT_BLOWFISH)
+			throw new CHttpException(500,"This application requires that PHP was compiled with Blowfish support for crypt().");
+
+		$model=new LoginForm;
+
+		// if it is ajax validation request
+		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+
+		// collect user input data
+		if(isset($_POST['LoginForm']))
+		{
+			$model->attributes=$_POST['LoginForm'];
+			// validate user input and redirect to the previous page if valid
+			if($model->validate() && $model->login())
+				$this->redirect(Yii::app()->user->returnUrl);
+		}
+		// display the login form
+		$this->render('login',array('model'=>$model));
     }
 
 
